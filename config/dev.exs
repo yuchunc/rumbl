7dc986c87f13973a2da664257c6566749bc660d4
Mickey <mchen@optimisdev.com>
2015-11-16 17:45:42 +0800

    working through chapter 4, Stoped before Building Forms

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "rumbl_dev",
   hostname: "localhost",
   pool_size: 10
--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,25 +1,3 @@
 defmodule Rumbl.Repo do
-  # use Ecto.Repo, otp_app: :rumbl
-  @moduledoc """
-  In memory repo.
-  """
-
-  def all(Rumbl.User) do
-    [%Rumbl.User{id: "1", name: "Mickey", username: "mickey", password: "geek"},
-      %Rumbl.User{id: "2", name: "Vicky", username: "vicky", password: "wife"},
-      %Rumbl.User{id: "3", name: "Xun", username: "xun", password: "son"},
-    ]
-  end
-
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
+  use Ecto.Repo, otp_app: :rumbl
 end
